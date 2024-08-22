e889ea4610b138147321b0e0f9dc8876ff50dd23
nthngspcl <atajpp6@gmail.com>
2024-08-22 23:48:00 +0600

    front v1

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <h1 className="navbar-title" onClick={() => handleNavigate('/')}>Yolo</h1>
         <div className="navbar-right">
           <a href='https://t.me/manager_temir' className="socialIcon"><img src={telegram} alt="telegram icon" className="social-icon"/></a>
-          <a href='https://web.whatsapp.com/' className="socialIcon"><img src={whatsapp} alt="whatsapp icon" className="social-icon"/></a>
+          <a href='http://wa.me/996702930787' className="socialIcon"><img src={whatsapp} alt="whatsapp icon" className="social-icon"/></a>
           <a href='https://dzen.ru/yolokg' className="socialIcon"><img src={yandexzen} alt="yandex zen icon" className="social-icon"/></a>
         </div>
       </div>
